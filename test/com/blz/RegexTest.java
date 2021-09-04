ba9c65e740c3cb6fcc9f4a9f009b862ac2025f94
Saurabh <saurabhdesale007@gmail.com>
2021-09-04 21:49:04 +0530

    Added: Done E-mail test cases provided by separately

--- test/com/blz/RegexTest.java
+++ test/com/blz/RegexTest.java
@@ -58,20 +58,129 @@ class RegexTest {
 	public void testPassInvalidGap_Fail() {
 		assertFalse(form.userPass("Cristaino 07@#"));
 	}
+
 	@Test
 	public void testPassInvalidAtleastEightCharacter_Fail() {
 		assertFalse(form.userPass("Cri07@#"));
 	}
+
 	@Test
 	public void testPassInvalidOneUpperCase_Fail() {
 		assertFalse(form.userPass("ristaino07@#"));
 	}
+
 	@Test
 	public void testPassInvalidAtLeastOneNumericNumber_Fail() {
 		assertFalse(form.userPass("Cristaino@#"));
 	}
+
 	@Test
 	public void testPassInvalidAtLeastOneSpecialCharacter_Fail() {
 		assertFalse(form.userPass("Cristaino012"));
+	}
+
+	@Test
+	public void testEmail10Valid_Success() {
+		assertTrue(form.userEmail("abc@yahoo.com"));
+	}
+
+	@Test
+	public void testEmail2Valid_Success() {
+		assertTrue(form.userEmail("abc-100@yahoo.com"));
+	}
+
+	@Test
+	public void testEmail3Valid_Success() {
+		assertTrue(form.userEmail("abc.100@yahoo.com"));
+	}
+
+	@Test
+	public void testEmail4Valid_Success() {
+		assertTrue(form.userEmail("abc111@abc.com"));
+	}
+
+	@Test
+	public void testEmail5Valid_Success() {
+		assertTrue(form.userEmail("abc-100@abc.net"));
+	}
+
+	@Test
+	public void testEmail6Valid_Success() {
+		assertTrue(form.userEmail("abc.100@abc.com.au"));
+	}
+
+	@Test
+	public void testEmail7Valid_Success() {
+		assertTrue(form.userEmail("abc@1.com"));
+	}
+
+	@Test
+	public void testEmail8Valid_Success() {
+		assertTrue(form.userEmail("abc@gmail.com.com"));
+	}
+
+	@Test
+	public void testEmail9Valid_Success() {
+		assertTrue(form.userEmail("abc+100@gmail.com"));
+	}
+
+	@Test
+	public void testEmail2Valid_Fail() {
+		assertFalse(form.userEmail("abc@.com.my"));
+	}
+
+	@Test
+	public void testEmail3Valid_Fail() {
+		assertFalse(form.userEmail("abc123@gmail.com.a"));
+	}
+
+	@Test
+	public void testEmail4Valid_Fail() {
+		assertFalse(form.userEmail("abc123@.com.com"));
+	}
+
+	@Test
+	public void testEmail5Valid_Fail() {
+		assertFalse(form.userEmail(".abc@abc.com"));
+	}
+
+	@Test
+	public void testEmail6Valid_Fail() {
+		assertFalse(form.userEmail("abc()*@gmail.com"));
+	}
+
+	@Test
+	public void testEmail7Valid_Fail() {
+		assertFalse(form.userEmail("abc@%*.com"));
+	}
+
+	@Test
+	public void testEmail8Valid_Fail() {
+		assertFalse(form.userEmail("abc..2002@gmail.com"));
+	}
+
+	@Test
+	public void testEmail9Valid_Fail() {
+		assertFalse(form.userEmail("abc.@gmail.com"));
+	}
+
+	@Test
+	public void testEmail10Valid_Fail() {
+		assertFalse(form.userEmail("abc@abc@gmail.com"));
+	}
+
+	@Test
+	public void testEmail11Valid_Fail() {
+		assertFalse(form.userEmail("abc@gmail.com.1a"));
+	}
+
+	@Test
+	public void testEmail12Valid_Fail() {
+		assertFalse(form.userEmail("abc@gmail.com.aa.au"));
+	}
+
+	@Test
+	public void testEmailValid_Fail() {
+		assertFalse(form.userEmail("abc"));
 	}
 }
